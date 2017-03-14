0869b07b4e3559e45ca8835571f8c0690e6669eb
chandrakr <chandrakr@countryday.net>
2017-03-14 10:43:27 -0400

    Update AddressBook.java
    Roshan Chandrakumar

--- AddressBook.java
+++ AddressBook.java
@@ -6,8 +6,10 @@ public class AddressBook {
     private Contact[] friends; // An array of Contacts - each stores info for one friend
     private int numfriends; // Number of friends currently in AddressBook
 
+	
     // Create an empty AddressBook
     public AddressBook() {
 		friends = new Contact[10];
 		numfriends = 0;
     }
+
